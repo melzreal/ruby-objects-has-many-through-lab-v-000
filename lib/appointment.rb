cd4283b4de62395240ac094a0f430e553a963419
Melissa Real <melissa.real@hotmail.com>
2018-08-13 15:03:59 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,11 +1,9 @@
-require 'pry'
 class Appointment
   attr_accessor :date, :patient, :doctor 
   @@all = [] 
   
   
-  
-  def initialize(patient, doctor, date)
+  def initialize(date, patient, doctor)
     @date = date 
     @patient = patient 
     @doctor = doctor 
